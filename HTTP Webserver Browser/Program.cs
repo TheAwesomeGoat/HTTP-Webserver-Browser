02e057ffbe5bcbbe2b7e14e4f19bed7e6d556f3b
TheAwesomeGoat <roelofrijken9@gmail.com>
2021-09-23 14:18:19 +0200

    Add project files.

---
+++ HTTP Webserver Browser/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HTTP_Webserver_Browser
+{
+    class Program
+    {
+        static SearchWebserver a = new SearchWebserver();
+        static string baseurl = "http://172.24.30.1:6969";
+        static void Main(string[] args)
+        {
+            var Folders = a.SearchFolders($"{baseurl}/");
+            foreach (var Folder in Folders)
+                LoadSubDirs(Folder);
+
+            var Files = a.SearchFiles($"{baseurl}/", ".mp3");
+
+            Console.ReadKey();
+        }
+
+        static void LoadSubDirs(string dir)
+        {
+            Console.WriteLine(dir);
+            var Folders = a.SearchFolders($"{baseurl}{dir}");
+            foreach (var Folder in Folders)
+                LoadSubDirs(Folder);
+        }
+    }
+}
---
+++ HTTP Webserver Browser/SearchWebserver.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_Webserver_Browser
+{
+    class SearchWebserver
+    {
+        private string sub(string str, int begin, int end)
+        {
+            return str.Substring(begin, end - begin);
+        }
+
+        private string Connect(string url)
+        {
+            using (WebClient client = new WebClient())
+                return client.DownloadString(url);
+        }
+
+        public List<string> SearchFolders(string url)
+        {
+            int HrefBegin = 0;
+            var Folders = new List<string>();
+            string data = Connect(url);
+            if (data != null)
+            {
+                while (true)
+                {
+                    HrefBegin = data.IndexOf("<a href=\"", HrefBegin + 1);
+                    int HrefEnd = data.IndexOf("</a>", HrefBegin + 1) + 4;
+                    int HrefDirEnd = data.IndexOf("\">", HrefBegin + 1);
+
+                    if (HrefBegin == -1)
+                        break;
+
+                    string FullHref = sub(data, HrefBegin, HrefEnd);
+                    string HrefDir = sub(data, HrefBegin + 9, HrefDirEnd);
+                    string HrefText = sub(data, HrefDirEnd + 2, HrefEnd - 4);
+
+                    if (!HrefDir.Contains("../") & HrefDir.EndsWith("/"))
+                        Folders.Add(HrefDir);
+                }
+            }
+            return Folders;
+        }
+        public List<string> SearchFiles(string url, string extension)
+        {
+            int HrefBegin = 0;
+            var Files = new List<string>();
+            string data = Connect(url);
+            if (data != null)
+            {
+                while (true)
+                {
+                    HrefBegin = data.IndexOf("<a href=\"", HrefBegin + 1);
+                    int HrefEnd = data.IndexOf("</a>", HrefBegin + 1) + 4;
+                    int HrefDirEnd = data.IndexOf("\">", HrefBegin + 1);
+
+                    if (HrefBegin == -1)
+                        break;
+
+                    string FullHref = sub(data, HrefBegin, HrefEnd);
+                    string HrefDir = sub(data, HrefBegin + 9, HrefDirEnd);
+                    string HrefText = sub(data, HrefDirEnd + 2, HrefEnd - 4);
+
+                    if (!HrefDir.Contains("../") & (HrefDir.EndsWith(extension) | extension == "*"))
+                        Files.Add(HrefDir);
+                }
+            }
+            return Files;
+        }
+    }
+}
